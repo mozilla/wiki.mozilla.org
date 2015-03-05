291a21fe4dd289b4d038cb4392731095aa4ffc86
Christie Koehler <ck@christi3k.net>
2015-03-04 18:03:14 -0800

    Remove patrol from confirm group, add to bureaucrat.

--- LocalSettings.php
+++ LocalSettings.php
@@ -242,7 +242,6 @@ $wgGroupPermissions['confirm']['reupload-shared']           = true;
 $wgGroupPermissions['confirm']['editsemiprotected']         = true;
 $wgGroupPermissions['confirm']['patrolmarks']               = true;
 $wgGroupPermissions['confirm']['autopatrol']                = true;
-$wgGroupPermissions['confirm']['patrol']                    = true;
 $wgGroupPermissions['confirm']['purge']                     = true;
 $wgGroupPermissions['confirm']['writeapi']                  = true;
 $wgGroupPermissions['confirm']['autoconfirmed']             = true;
@@ -284,7 +283,7 @@ $wgGroupPermissions['bureaucrat']['autoconfirmed']          = true;
 $wgGroupPermissions['bureaucrat']['nuke']                   = false;
 $wgGroupPermissions['bureaucrat']['delete']                 = false;
 $wgGroupPermissions['bureaucrat']['rollback']               = false;
-$wgGroupPermissions['bureaucrat']['patrol']                 = false;
+$wgGroupPermissions['bureaucrat']['patrol']                 = true;
 
 // members of sysop group
 $wgGroupPermissions['sysop']['createaccount']         = true;
