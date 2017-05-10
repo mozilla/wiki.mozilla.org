7a2a746c1462b3f1daf507d6c3b9ae3fdcec80f2
Mark A. Hershberger <mah@everybody.org>
2017-05-10 15:43:33 -0400

    extensions update

--- LocalSettings.php
+++ LocalSettings.php
@@ -50,9 +50,6 @@ $wgMetaNamespace = "MozillaWiki";
 ## For more information on customizing the URLs
 ## (like /w/index.php/Page_title to /wiki/Page_title) please see:
 ## https://www.mediawiki.org/wiki/Manual:Short_URL
-$wgScriptPath = '';
-$wgArticlePath = '/$1';
-$wgUsePathInfo = false;
 $wgScriptPath = $SECRETS_wgScriptPath;
 $wgArticlePath = $SECRETS_wgArticlePath;
 $wgScript = $wgScriptPath . '/index.php';
@@ -71,7 +68,7 @@ $wgUploadPath = "{$wgScriptPath}/images";
 ## The relative URL path to the logo.  Make sure you change this from the default,
 ## or else you'll overwrite your logo when you upgrade!
 #$wgLogo = "$wgStylePath/common/images/$SECRETS_wgLogo";
-$wgLogo = "$wgStylePath/assets/logos/$SECRETS_wgLogo";
+$wgLogo = "$wgStylePath/common/assets/logos/$SECRETS_wgLogo";
 
 # The relative URL path to the favicon
 $wgFavicon = "$wgStylePath/common/assets/favicon.ico";
@@ -459,19 +456,20 @@ $wgDefaultUserOptions['wikieditor-preview'] = 1;
 // bug 1080898
 $wgDefaultUserOptions['forceeditsummary'] = true;
 
+#require_once("$IP/extensions/Urchin/Urchin.php");
 wfLoadExtension( 'LabeledSectionTransclusion' );
 wfLoadExtension( 'Renameuser' );
 wfLoadExtension( 'ParserFunctions' );
 wfLoadExtension( 'ImageMap' );
 
 $wgFFmpegLocation = '/usr/bin/ffmpeg';
-require_once("$IP/extensions/OggHandler/OggHandler.php");
+require("$IP/extensions/OggHandler/OggHandler.php");
 
 $smwgNamespaceIndex = 132;
 $smwgQMaxSize = 40;
 $smwgQMaxDepth = 20;
 
-require_once("$IP/../extensions/SemanticMediaWiki/SemanticMediaWiki.php");
+require_once("$IP/extensions/SemanticMediaWiki/SemanticMediaWiki.php");
 enableSemantics('wiki-dev.allizom.org');
 $smwgEnabledEditPageHelp = false;
 wfLoadExtension( 'PageForms' );
@@ -495,7 +493,7 @@ require_once("$IP/extensions/NoTitle/NoTitle.php");
 wfLoadExtension( 'InputBox' );
 
 # Bug 675064
-#require_once("$IP/../extensions/SemanticWatchlist/SemanticWatchlist.php");
+#require_once("$IP/extensions/SemanticWatchlist/SemanticWatchlist.php");
 
 # Bug 721366 and 731672
 require_once("$IP/extensions/Bugzilla/Bugzilla.php");
@@ -511,9 +509,9 @@ $wgCaptchaTriggers['create']        = false;
 $wgCaptchaTriggers['addurl']        = false;
 $wgCaptchaTriggers['createaccount'] = true;
 $wgCaptchaTriggers['badlogin']      = true;
-$wgCaptchaClass = 'ReCaptchaNoCaptcha';
-$wgReCaptchaSiteKey = $SECRETS_wgReCaptchaPublicKey;
-$wgReCaptchaSecretKey = $SECRETS_wgReCaptchaPrivateKey;
+$wgCaptchaClass = 'ReCaptcha';
+$wgReCaptchaPublicKey = $SECRETS_wgReCaptchaPublicKey;
+$wgReCaptchaPrivateKey = $SECRETS_wgReCaptchaPrivateKey;
 
 # bug 832030
 require_once("$IP/extensions/googleAnalytics/googleAnalytics.php");
@@ -638,9 +636,9 @@ require_once("$IP/extensions/Sandstone/Sandstone.php");
 // Bug 1082298
 $wgPFEnableStringFunctions = true;
 
-require_once("$IP/../extensions/Widgets/Widgets.php");
+require_once("$IP/extensions/Widgets/Widgets.php");
 
-require_once("$IP/../extensions/MobileFrontend/MobileFrontend.php");
+wfLoadExtension( 'MobileFrontend' );
 
 $wgMFAutodetectMobileView = true;
 $wgMobileFrontendLogo = "$wgStylePath/../assets/logos/$SECRETS_wgMobileFrontendLogo";
@@ -649,7 +647,7 @@ $wgMobileUrlTemplate = 'm.%h0.%h1.%h2';
 // must disable jquery table on legacy mediawiki-bugzilla extension for mobile editing to work
 $wgBugzillaJqueryTable = false;
 
-require_once("$IP/extensions/ImportFromEtherpad/ImportFromEtherpad.php");
+require_once "$IP/extensions/ImportFromEtherpad/ImportFromEtherpad.php";
 
 // these regexs replace strings in the converted mediawiki content
 $wgImportFromEtherpadSettings->contentRegexs[] = array("\n\n","\n");
