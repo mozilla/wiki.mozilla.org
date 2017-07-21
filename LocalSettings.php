e2503d1c4ee9de04024b4d79048895f90c4df5ce
eziegenhorn <eric@ziegenhorns.com>
2017-07-21 12:39:04 -0500

    Massage secrets

--- LocalSettings.php
+++ LocalSettings.php
@@ -16,22 +16,22 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 }
 
 # Source the nubis secrets file
-require_once('/etc/nubis-config/wiki.mozilla.org')
+require_once('/etc/nubis-config/wiki.php')
 #$SECRETS_wgServer                 = '';
-#$SECRETS_wgLogo                   = 'mozilla-wiki-logo-alt-135px.png';
+$SECRETS_wgLogo                   = 'mozilla-wiki-logo-alt-135px.png';
 #$SECRETS_wgDBserver               = '{{ getv "/config/Database/Server" }}';
 #$SECRETS_wgDBname                 = '{{ getv "/config/Database/Name" }}';
 #$SECRETS_wgDBuser                 = '{{ getv "/config/Database/User" }}';
 #$SECRETS_wgDBpassword             = '{{ getv "/config/Database/Password" }}';
-#$SECRETS_wgSecretKey              = '{{ getv "/config/Database/SecretKey" }}';
-#$SECRETS_wgUpgradeKey             = '{{ getv "/config/Database/UpgradeKey" }}';
-#$SECRETS_wgReCaptchaPublicKey     = '{{ getv "/config/Database/ReCatchpaPublicKey" }}';
-#$SECRETS_wgReCaptchaPrivateKey    = '{{ getv "/config/Database/ReCatchpaPrivateKey" }}';
+#$SECRETS_wgSecretKey              = '{{ getv "/config/SecretKey" }}';
+#$SECRETS_wgUpgradeKey             = '{{ getv "/config/UpgradeKey" }}';
+#$SECRETS_wgReCaptchaPublicKey     = '{{ getv "/config/ReCatchpaPublicKey" }}';
+#$SECRETS_wgReCaptchaPrivateKey    = '{{ getv "/config/ReCatchpaPrivateKey" }}';
 #$SECRETS_wgGoogleAnalyticsAccount = 'UA-35433268-22';
 ##$SECRETS_wgSquidServers           = array('192.168.1.1', '192.168.1.2');
 ##$SECRETS_wgMemCachedServers       = array();
-#$SECRETS_wgUploadDirectory       = "/var/www/wiki.mozilla.org/images";
-#$SECRETS_wgMobileFrontendLogo     = "mozilla-wiki-logomark-35px.png";
+$SECRETS_wgUploadDirectory       = "/var/www/wiki/images";
+$SECRETS_wgMobileFrontendLogo     = "mozilla-wiki-logomark-35px.png";
 
 ## Source the secrets settings file
 ## If environment variables are set we will use a secrets file that
