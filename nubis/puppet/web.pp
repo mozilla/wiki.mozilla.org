3f60daa7dba4b36348a299ae2ea9895db3874e4d
eziegenhorn <eric@ziegenhorns.com>
2017-07-21 10:50:51 -0500

    Change project_name to wiki and try to use it almost everywhere

--- nubis/puppet/web.pp
+++ nubis/puppet/web.pp
@@ -19,7 +19,7 @@ nubis::configuration{ $project_name:
 apache::vhost { $project_name:
     port               => 80,
     default_vhost      => true,
-    docroot            => "/var/www/wiki.mozilla.org/core",
+    docroot            => "/var/www/$project_name/core",
     docroot_owner      => 'root',
     docroot_group      => 'root',
     block              => ['scm'],
@@ -31,9 +31,9 @@ apache::vhost { $project_name:
     access_log_env_var => '!internal',
     access_log_format  => '%a %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"',
     custom_fragment    => "
-    # Don't set default expiry on anything
-    ExpiresActive Off
-",
+        # Don't set default expiry on anything
+        ExpiresActive Off
+    ",
     headers            => [
       # Nubis headers
       "set X-Nubis-Version ${project_version}",
@@ -48,81 +48,82 @@ apache::vhost { $project_name:
     ],
 
     rewrites           => [
+#      {
+#        #    RewriteRule ^/AdminWiki(/.*|$) https://intranet.mozilla.org/%{QUERY_STRING} [R=permanent,L]
+#        comment      => 'Rewrite the old UseMod URLs to the new MediaWiki ones',
+#        rewrite_rule => ['^/AdminWiki(/.*|$) https://intranet.mozilla.org/%{QUERY_STRING} [R=permanent,L]'],
+#      },
+#      {
+#        #    RewriteRule ^/PluginFutures(/.*|$) https://intranet.mozilla.org/PluginFutures$1 [R=permanent,L]
+#        comment      => 'Rewrite the old UseMod URLs to the new MediaWiki ones',
+#        rewrite_rule => ['^/PluginFutures(/.*|$) https://intranet.mozilla.org/PluginFutures$1 [R=permanent,L]'],
+#      },
+#      {
+#        #    RewriteRule ^/ECMA(/.*|$) https://intranet.mozilla.org/ECMA$1 [R=permanent,L]
+#        comment      => 'This is for the ECMAScript 4 working group bug 324452',
+#        rewrite_rule => ['^/ECMA(/.*|$) https://intranet.mozilla.org/ECMA$1 [R=permanent,L]'],
+#      },
+#      {
+#        #    RewriteRule ^/Mozilla2\.0([/\?].*|$) /wiki/Mozilla2:Home_Page? [R,L]
+#        comment      => 'Old Wiki that has been moved into the public wiki',
+#        rewrite_rule => ['^/Mozilla2\.0([/\?].*|$) /wiki/Mozilla2:Home_Page? [R,L]'],
+#      },
+#      {
+#        #    RewriteRule ^/GeckoDev([/\?].*|$) /wiki/GeckoDev:Home_Page? [R,L]
+#        comment      => 'Old Wiki that has been moved into the public wiki',
+#        rewrite_rule => ['^/GeckoDev([/\?].*|$) /wiki/GeckoDev:Home_Page? [R,L]'],
+#      },
+#      {
+#        #    RewriteRule ^/XULDev([/\?].*|$) /wiki/XUL:Home_Page? [R,L]
+#        comment      => 'Old Wiki that has been moved into the public wiki',
+#        rewrite_rule => ['^/XULDev([/\?].*|$) /wiki/XUL:Home_Page? [R,L]'],
+#      },
+#      {
+#        #    RewriteRule ^/Calendar([/\?].*|$) /wiki/Calendar:Home_Page? [R,L]
+#        comment      => 'Old Wiki that has been moved into the public wiki',
+#        rewrite_rule => ['^/Calendar([/\?].*|$) /wiki/Calendar:Home_Page? [R,L]'],
+#      },
+#      {
+#        #    RewriteRule ^/SVG([/\?].*|$) /wiki/SVG:Home_Page? [R,L]
+#        comment      => 'Old Wiki that has been moved into the public wiki',
+#        rewrite_rule => ['^/SVG([/\?].*|$) /wiki/SVG:Home_Page? [R,L]'],
+#      },
+#      {
+#        #    RewriteRule ^/SVGDev([/\?].*|$) /wiki/SVGDev:Home_Page? [R,L]
+#        comment      => 'Old Wiki that has been moved into the public wiki',
+#        rewrite_rule => ['^/SVGDev([/\?].*|$) /wiki/SVGDev:Home_Page? [R,L]'],
+#      },
+#      {
+#        #    RewriteRule ^/mozwiki https://wiki.mozilla.org/ [R,L]
+#        comment      => 'Old Wiki that has been moved into the public wiki',
+#        rewrite_rule => ['^/mozwiki https://wiki.mozilla.org/ [R,L]'],
+#      },
+#      {
+#        #    RewriteRule ^/wiki/(.*)$ https://wiki.mozilla.org/$1 [R,L]
+#        comment      => 'Redirect old /wiki/ urls',
+#        rewrite_rule => ['^/wiki/(.*)$ https://wiki.mozilla.org/$1 [R,L]'],
+#      },
+#      {
+#        #    RewriteRule ^/wiki$ https://wiki.mozilla.org/index.php [R,L]
+#        comment      => 'Redirect old /wiki/ urls',
+#        rewrite_rule => ['^/wiki$ https://wiki.mozilla.org/index.php [R,L]'],
+#      },
       {
-        #    RewriteRule ^/AdminWiki(/.*|$) https://intranet.mozilla.org/%{QUERY_STRING} [R=permanent,L]
-        comment      => 'Rewrite the old UseMod URLs to the new MediaWiki ones',
-        rewrite_rule => ['^/AdminWiki(/.*|$) https://intranet.mozilla.org/%{QUERY_STRING} [R=permanent,L]'],
-      },
-      {
-        #    RewriteRule ^/PluginFutures(/.*|$) https://intranet.mozilla.org/PluginFutures$1 [R=permanent,L]
-        comment      => 'Rewrite the old UseMod URLs to the new MediaWiki ones',
-        rewrite_rule => ['^/PluginFutures(/.*|$) https://intranet.mozilla.org/PluginFutures$1 [R=permanent,L]'],
-      },
-      {
-        #    RewriteRule ^/ECMA(/.*|$) https://intranet.mozilla.org/ECMA$1 [R=permanent,L]
-        comment      => 'This is for the ECMAScript 4 working group bug 324452',
-        rewrite_rule => ['^/ECMA(/.*|$) https://intranet.mozilla.org/ECMA$1 [R=permanent,L]'],
-      },
-      {
-        #    RewriteRule ^/Mozilla2\.0([/\?].*|$) /wiki/Mozilla2:Home_Page? [R,L]
-        comment      => 'Old Wiki that has been moved into the public wiki',
-        rewrite_rule => ['^/Mozilla2\.0([/\?].*|$) /wiki/Mozilla2:Home_Page? [R,L]'],
-      },
-      {
-        #    RewriteRule ^/GeckoDev([/\?].*|$) /wiki/GeckoDev:Home_Page? [R,L]
-        comment      => 'Old Wiki that has been moved into the public wiki',
-        rewrite_rule => ['^/GeckoDev([/\?].*|$) /wiki/GeckoDev:Home_Page? [R,L]'],
-      },
-      {
-        #    RewriteRule ^/XULDev([/\?].*|$) /wiki/XUL:Home_Page? [R,L]
-        comment      => 'Old Wiki that has been moved into the public wiki',
-        rewrite_rule => ['^/XULDev([/\?].*|$) /wiki/XUL:Home_Page? [R,L]'],
-      },
-      {
-        #    RewriteRule ^/Calendar([/\?].*|$) /wiki/Calendar:Home_Page? [R,L]
-        comment      => 'Old Wiki that has been moved into the public wiki',
-        rewrite_rule => ['^/Calendar([/\?].*|$) /wiki/Calendar:Home_Page? [R,L]'],
-      },
-      {
-        #    RewriteRule ^/SVG([/\?].*|$) /wiki/SVG:Home_Page? [R,L]
-        comment      => 'Old Wiki that has been moved into the public wiki',
-        rewrite_rule => ['^/SVG([/\?].*|$) /wiki/SVG:Home_Page? [R,L]'],
-      },
-      {
-        #    RewriteRule ^/SVGDev([/\?].*|$) /wiki/SVGDev:Home_Page? [R,L]
-        comment      => 'Old Wiki that has been moved into the public wiki',
-        rewrite_rule => ['^/SVGDev([/\?].*|$) /wiki/SVGDev:Home_Page? [R,L]'],
-      },
-      {
-        #    RewriteRule ^/mozwiki https://wiki.mozilla.org/ [R,L]
-        comment      => 'Old Wiki that has been moved into the public wiki',
-        rewrite_rule => ['^/mozwiki https://wiki.mozilla.org/ [R,L]'],
-      },
-      {
-        #    RewriteRule ^/wiki/(.*)$ https://wiki.mozilla.org/$1 [R,L]
-        comment      => 'Redirect old /wiki/ urls',
-        rewrite_rule => ['^/wiki/(.*)$ https://wiki.mozilla.org/$1 [R,L]'],
-      },
-      {
-        #    RewriteRule ^/wiki$ https://wiki.mozilla.org/index.php [R,L]
-        comment      => 'Redirect old /wiki/ urls',
-        rewrite_rule => ['^/wiki$ https://wiki.mozilla.org/index.php [R,L]'],
-      },
-      {
-        #    RewriteCond %{REQUEST_URI} !^/(assets|extensions|images|skins|resources)/
-        #    RewriteCond %{REQUEST_URI} !^/(redirect|index|opensearch_desc|api|load|thumb).php
-        #    RewriteCond %{REQUEST_URI} !^/error/(40(1|3|4)|500).html
-        #    RewriteCond %{REQUEST_URI} !^/favicon.ico
-        #    RewriteCond %{REQUEST_URI} !^/robots.txt
-        #    RewriteRule ^/(.*)$ /data/www/wiki.mozilla.org/core/index.php?title=$1 [L,QSA]
+#        #    RewriteCond %{REQUEST_URI} !^/(assets|extensions|images|skins|resources)/
+#        #    RewriteCond %{REQUEST_URI} !^/(redirect|index|opensearch_desc|api|load|thumb).php
+#        #    RewriteCond %{REQUEST_URI} !^/error/(40(1|3|4)|500).html
+#        #    RewriteCond %{REQUEST_URI} !^/favicon.ico
+#        #    RewriteCond %{REQUEST_URI} !^/robots.txt
+#        #    RewriteRule ^/(.*)$ /data/www/wiki.mozilla.org/core/index.php?title=$1 [L,QSA]
         comment      => 'Do not rewrite requests for files in MediaWiki subdirectories, MediaWiki PHP files, HTTP error documents, favicon.ico, or robots.txt',
-        rewrite_cond => ['%{REQUEST_URI} !^/(assets|extensions|images|skins|resources)/'],
-        rewrite_cond => ['%{REQUEST_URI} !^/(redirect|index|opensearch_desc|api|load|thumb).php'],
-        rewrite_cond => ['%{REQUEST_URI} !^/error/(40(1|3|4)|500).html'],
-        rewrite_cond => ['%{REQUEST_URI} !^/favicon.ico'],
-        rewrite_cond => ['%{REQUEST_URI} !^/robots.txt'],
-        # Rewrite http://wiki.domain.tld/article properly, this is the main rule
-        rewrite_rule => ['^/(.*)$ /data/www/wiki.mozilla.org/core/index.php?title=$1 [L,QSA]'],
+#        rewrite_cond => ['%{REQUEST_URI} !^/(assets|extensions|images|skins|resources)/'],
+#        rewrite_cond => ['%{REQUEST_URI} !^/(redirect|index|opensearch_desc|api|load|thumb).php'],
+#        rewrite_cond => ['%{REQUEST_URI} !^/error/(40(1|3|4)|500).html'],
+#        rewrite_cond => ['%{REQUEST_URI} !^/favicon.ico'],
+#        rewrite_cond => ['%{REQUEST_URI} !^/robots.txt'],
+#        # Rewrite http://wiki.domain.tld/article properly, this is the main rule
+        rewrite_rule => ["^/(.*)$ /var/www/wiki/core/index.php?title=$1 [L,QSA]"],
+        # Todo change wiki to $project_name once this is working
       },
     ],
 
--- nubis/puppet/wiki.pp
+++ nubis/puppet/wiki.pp
@@ -2,45 +2,45 @@
 include mysql::client
 
 package { "php5-mysql":
-  ensure => '5.5.9+dfsg-1ubuntu4.21'
+  ensure => 'latest'
 }
 
 package { 'php5-xcache':
-  ensure => '3.1.0-2'
+  ensure => 'latest'
 }
 
 package { 'graphviz':
-  ensure => '2.36.0-0ubuntu3.2'
+  ensure => 'latest'
 }
 
 package { 'php5-memcache':
-  ensure => '3.0.8-4build1'
+  ensure => 'latest'
 }
 
 package { 'php5-intl':
-  ensure => '5.5.9+dfsg-1ubuntu4.21'
+  ensure => 'latest'
 }
 
 package { 'php5-gd':
-  ensure => '5.5.9+dfsg-1ubuntu4.21'
+  ensure => 'latest'
 }
 
 package { 'pandoc':
-  ensure => '1.12.2.1-1build2'
+  ensure => 'latest'
 }
 
 package { 'memcached':
-  ensure => '1.4.14-0ubuntu9.1'
+  ensure => 'latest'
 }
 
 package { 'librsvg2-bin':
-  ensure => '2.40.2-1'
+  ensure => 'latest'
 }
 
 package { 'imagemagick':
-  ensure => '8:6.7.7.10-6ubuntu3.7'
+  ensure => 'latest'
 }
 
-package { 'libapache2-mod-php5':
-  ensure => latest;
+package { 'libapache2-mod-php':
+  ensure => 'latest';
 }
