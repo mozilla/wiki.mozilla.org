32adf7d58f0e6aa9f77ae746cd295af7d05e3d79
eziegenhorn <eric@ziegenhorns.com>
2017-07-28 15:23:03 -0500

    Move as much from update.sh to puppet as we can so we only do it at build time

--- nubis/puppet/wiki.pp
+++ nubis/puppet/wiki.pp
@@ -45,12 +45,6 @@ package { 'git':
   ensure => 'latest'
 }
 
-# Superceded by apache::mod:php we suspect
-#package { 'libapache2-mod-php5':
-#  ensure => 'latest';
-#}
-
-
 # Generate some random secrets for mediawiki on boot
 file { '/etc/nubis.d/wiki-secrets':
     ensure => file,
@@ -59,3 +53,64 @@ file { '/etc/nubis.d/wiki-secrets':
     mode   => '0755',
     source => 'puppet:///nubis/files/wiki-onboot',
 }
+
+# Set up cache directory
+exec { 'wiki_cache_dir':
+    command => 'mkdir -p /var/tmp/wikimo-cache; chown -R www-data /var/tmp/wikimo-cache',
+}
+
+# Set permissions for Widgets
+exec { 'widgets_permissions':
+    command => 'chown -R www-data extensions/Widgets/compiled_templates',
+}
+
+# Link files that aren't in the core repo to where they need to be
+file { '/var/www/$project_name/core/LocalSettings.php':
+    ensure => 'link',
+    target => '/var/www/$project_name/LocalSettings.php',
+}
+file { '/var/www/$project_name/core/composer.local.json':
+    ensure => 'link',
+    target => '/var/www/$project_name/composer.local.json',
+}
+exec { 'link_extensions':
+    command => 'cd /var/www/$project_name; for ext in $(find extensions -maxdepth 1 -mindepth 1 -type d); do link core/$ext ../../$ext; done',
+}
+exec { 'link_skins':
+    command => 'cd /var/www/$project_name; for skin in $(find skins -maxdepth 1 -mindepth 1 -type d); do link core/$skin ../../$skin; done',
+}
+
+# Links to EFS mount dirs
+file { '/var/www/$project_name/images':
+    ensure => 'link',
+    target => '/data/$project_name',
+}
+file { '/var/www/$project_name/images':
+    ensure => 'link',
+    target => '/data/$project_name/images',
+}
+file { '/var/www/$project_name/php_sessions':
+    ensure => 'link',
+    target => '/data/$project_name/php_sessions',
+}
+file { '/var/www/$project_name/extensions/Bugzilla/charts':
+    ensure => 'link',
+    target => '/data/$project_name/Bugzilla_charts',
+}
+
+# Install PHP composer extensions
+exec { 'composer':
+    command => 'cd /var/www/$project_name/core && php ../tools/composer.phar install --no-dev &&; php ../tools/composer.phar update --no-dev',
+}
+
+# Localization
+exec { 'localize':
+    command => 'cd /var/www/$project_name && php maintenance/rebuildLocalisationCache.php',
+}
+
+# DB migratation
+exec { 'migration':
+    command => 'cd /var/www/$project_name && php maintenance/update.php --quick',
+    require => Exec['composer'],
+}
+
