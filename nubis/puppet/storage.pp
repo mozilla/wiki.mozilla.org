0c6c921aed05daebb750fd45ced4f084cf876650
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-08-03 14:15:45 -0400

    fix quoteing

--- nubis/puppet/storage.pp
+++ nubis/puppet/storage.pp
@@ -9,16 +9,16 @@ nubis::storage { $project_name:
 ### puppet-nubis-storage
 
 # Link to our mountpoints
-file { '/var/www/${project_name}':
+file { "/var/www/${project_name}":
   ensure => 'directory',
   force  => true,
 }
-file { '/var/www/${project_name}/images':
+file { "/var/www/${project_name}/images":
   ensure => 'link',
   force  => true,
   target => "/data/${project_name}/images",
 }
-file { '/var/www/${project_name}/php_sessions':
+file { "/var/www/${project_name}/php_sessions":
   ensure => 'link',
   force  => true,
   target => "/data/${project_name}/php_sessions",
