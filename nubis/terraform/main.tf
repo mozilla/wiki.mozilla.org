0e4f6443333b40c0b59279ecf9fb25fb423992ce
eziegenhorn <eric@ziegenhorns.com>
2017-08-04 13:33:54 -0500

    Increase instance size for more memory

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -11,6 +11,7 @@ module "worker" {
   ssh_key_name      = "${var.ssh_key_name}"
   nubis_sudo_groups = "${var.nubis_sudo_groups}"
   nubis_user_groups = "${var.nubis_user_groups}"
+  instance_type     = "t2.small"
   health_check_type = "EC2"     # EC2 or ELB
 }
 
