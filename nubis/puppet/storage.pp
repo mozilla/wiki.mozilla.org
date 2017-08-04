1a80a9e71dcba0bac58fa7015e8497a028b9d410
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-08-03 21:45:17 -0400

    Update storage.pp

--- nubis/puppet/storage.pp
+++ nubis/puppet/storage.pp
@@ -5,21 +5,3 @@ nubis::storage { $project_name:
   owner => 'www-data',
   group => 'www-data',
 }
-
-### puppet-nubis-storage
-
-# Link to our mountpoints
-file { "/var/www/${project_name}":
-  ensure => 'directory',
-  force  => true,
-}
-file { "/var/www/${project_name}/images":
-  ensure => 'link',
-  force  => true,
-  target => "/data/${project_name}/images",
-}
-file { "/var/www/${project_name}/php_sessions":
-  ensure => 'link',
-  force  => true,
-  target => "/data/${project_name}/php_sessions",
-}
