11101e84e8f4e6250be73f69ffb9d91f061f9cab
eziegenhorn <eric@ziegenhorns.com>
2017-09-07 12:07:54 -0500

    Specify custom ssl cert

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -22,6 +22,7 @@ module "load_balancer" {
   account      = "${var.account}"
   service_name = "${var.service_name}"
   health_check_target = "HTTP:80/?redirect=0"
+  ssl_cert_name_prefix = "${var.service_name}"
 }
 
 module "dns" {
