4371796548e7cbe315834801fced3d6c2242b948
eziegenhorn <eric@ziegenhorns.com>
2017-10-29 23:06:12 -0500

    Increase ELB idle_timeout to deal with Bugzilla extension waiting for long BMO queries

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -26,6 +26,7 @@ module "load_balancer" {
   health_check_target = "HTTP:80/?redirect=0"
   ssl_cert_name_prefix = "${var.service_name}"
   health_check_timeout = 5
+  idle_timeout = 300
 }
 
 module "dns" {
