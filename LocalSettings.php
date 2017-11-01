6e43d9f1a40976d9eff0df84fb1b306f9fad703e
eziegenhorn <eric@ziegenhorns.com>
2017-11-01 16:59:50 -0500

    Try reverting bugzilla caching

--- LocalSettings.php
+++ LocalSettings.php
@@ -498,8 +498,6 @@ wfLoadExtension( 'InputBox' );
 
 # Bug 721366 and 731672
 require_once("$IP/extensions/Bugzilla/Bugzilla.php");
-# Cache bugzilla data for 5 minutes
-$wgBugzillaCacheTimeOut = 5;
 
 # Bug 772192 & 838391
 require_once("$IP/extensions/Smartsheet-MediaWiki-Extension/SmartsheetIframe.php");
