821c69fd315b20e9cdc265b9441acb89554f845a
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-11-10 11:21:51 -0500

    move EFS data into s3 daily

--- nubis/puppet/storage.pp
+++ nubis/puppet/storage.pp
@@ -5,3 +5,19 @@ nubis::storage { $project_name:
   owner => 'www-data',
   group => 'www-data',
 }
+
+file { "/usr/local/bin/${project_name}-backup":
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+    source => 'puppet:///nubis/files/backup',
+}
+
+cron::daily { "${project_name}-backup":
+  command => "/usr/local/bin/${project_name}-backup /data/$project_name",
+  # Pick a time between 0-5 hour
+  hour => fqdn_rand(6)
+  # Pick a random minute
+  minute => fqdn_rand(60),
+}
