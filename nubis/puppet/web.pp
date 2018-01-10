b73bfcabb54f15c3aa0a5cd4aab8703d89e39440
Daniel Hartnell <danielhartnell@gmail.com>
2018-01-10 14:18:46 -0800

    second install of New Relic with proper apt repo release

--- nubis/puppet/web.pp
+++ nubis/puppet/web.pp
@@ -33,6 +33,7 @@ apache::vhost { $project_name:
 
 	# Compress custom deflate types
 	Include /etc/apache2/mods-enabled/deflate.conf
+  Include /etc/apache2/mods-enabled/newrelic.conf
 	AddOutputFilterByType DEFLATE text/javascript
     ",
     headers            => [
