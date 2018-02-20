c43e51c4008eb1e43c96e6646f06461544883162
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-02-20 10:36:15 -0500

    Fix cronjob lock

--- nubis/puppet/storage.pp
+++ nubis/puppet/storage.pp
@@ -15,7 +15,7 @@ file { "/usr/local/bin/${project_name}-backup":
 }
 
 cron::daily { "${project_name}-backup":
-  command => "consul-do ${project_name}-backup $(hostname) && nubis-cron ${project_name}-backup /usr/local/bin/${project_name}-backup /data/$project_name 2>&1 | logger -t ${project_name}-backup",
+  command => "consul-do $(nubis-metadata NUBIS_PROJECT)-$(nubis-metadata NUBIS_ENVIRONMENT)-backup $(hostname) && nubis-cron ${project_name}-backup /usr/local/bin/${project_name}-backup /data/${project_name} 2>&1 | logger -t ${project_name}-backup",
   # Pick a time between 0-5 hour
   hour => fqdn_rand(6),
   # Pick a random minute
