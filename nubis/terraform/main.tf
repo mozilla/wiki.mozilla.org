e16d4f4af5b7dfbf2cb031971c964d2b67ded25e
Philippe M. Chiasson <gozer@ectoplasm.org>
2019-05-30 16:35:03 -0400

    shrink wiki stage instances

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -20,9 +20,9 @@ module "worker" {
   ssh_key_name      = "${var.ssh_key_name}"
   nubis_sudo_groups = "${var.nubis_sudo_groups}"
   nubis_user_groups = "${var.nubis_user_groups}"
-  instance_type     = "m3.medium"
-  health_check_type = "ELB"     # EC2 or ELB
-  min_instances	    = "${var.environment == "prod" ? 5 : 3}"
+  instance_type     = "${var.environment == "prod" ? "m5.medium" : "t3.small"}"
+  health_check_type = "ELB"                                                        # EC2 or ELB
+  min_instances     = "${var.environment == "prod" ? 5 : 3}"
 
   # CPU utilisation based autoscaling
   scale_down_load = 30
