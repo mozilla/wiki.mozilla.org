d41e71da9ff922ec804fca44a1159316aa087fe5
Philippe M. Chiasson <gozer@ectoplasm.org>
2019-05-30 17:15:39 -0400

    better sizing of BD

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -57,9 +57,9 @@ module "database" {
   monitoring             = true
   service_name           = "${var.service_name}"
   client_security_groups = "${module.worker.security_group}"
-  allocated_storage      = 20
+  allocated_storage      = 40
   multi_az               = true
-  instance_class         = "db.r3.large"
+  instance_class         = "${var.environment == "prod" ? "db.r3.large" : "db.t2.large"}
   nubis_sudo_groups      = "${var.nubis_sudo_groups},team_dbeng"
 }
 
