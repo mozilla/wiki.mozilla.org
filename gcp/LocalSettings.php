43ee1b5c1c681dbfbcd77583c960fa5bad429b12
Cassandra <cdymock@mozilla.com>
2024-09-12 09:38:30 -0700

    add tmp dir.

--- gcp/LocalSettings.php
+++ gcp/LocalSettings.php
@@ -94,6 +94,7 @@ $wgShellLocale = "C.UTF-8";
 ## to make your wiki go slightly faster. The directory should not
 ## be publically accessible from the web.
 $wgCacheDirectory = "/var/tmp/cache";
+$wgTmpDirectory = "/var/tmp/cache/tmp";
 
 # Site language code, should be one of the list in ./languages/data/Names.php
 $wgLanguageCode = "en";
